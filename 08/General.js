8f21d516519a3e64e76c32975e276734b06c33b8
Blake Bradford <bbradforddesign@gmail.com>
2020-10-17 23:18:16 -0500

    moved tables within General

--- 08/General.js
+++ 08/General.js
@@ -1,49 +1,50 @@
-// pointers to bases of memory segments.
-// NOTE: pointers with strings are initialized at runtime. pointers with number addresses are predefined.
-const Segments = {
-    sp: 'SP',
-    local: 'LCL',
-    argument: 'ARG',
-    this: 'THIS',
-    that: 'THAT',
-    temp: 5,
-    pointer: 3,
-    static: 16
-  }
-  
-  // assembly code presets for readability
-  const Macros = {
-    getTop: ["@SP",
-    "AM=M-1",
-    "D=M",],
-    putTop: ["@SP",
-    "M=M+1",
-    "A=M-1",
-    "M=D",],
-    gotoTop: ["@SP", 
-    "A=M-1", ],
-    terminate: ["(END)",
-    "@END",
-    "0;JMP"],
-    compare: (comparison, iterator) => {
-      return [
-        "D=M-D",
-        `@LT_${iterator}`,
-        `D;${comparison}`,
-        `@GT_${iterator}`,
-        "0;JMP",
-        `(LT_${iterator})`,
-        "D=-1",
-        `@STORE_${iterator}`,
-        "0;JMP",
-        `(GT_${iterator})`,
-        "D=0",
-        `(STORE_${iterator})`,
-      ]
-    }
-  }
+/**
+ * General tables used by multiple functions and processes
+ */
+module.exports = {
 
-  module.exports = {
-      Segments: Segments,
-      Macros: Macros
+    // pointers to bases of memory segments.
+    // NOTE: pointers with strings are initialized at runtime. pointers with number addresses are predefined.
+      Segments: {
+        sp: 'SP',
+        local: 'LCL',
+        argument: 'ARG',
+        this: 'THIS',
+        that: 'THAT',
+        temp: 5,
+        pointer: 3,
+        static: 16
+      },
+
+      // assembly code presets for readability
+      Macros: {
+        getTop: ["@SP",
+        "AM=M-1",
+        "D=M",],
+        putTop: ["@SP",
+        "M=M+1",
+        "A=M-1",
+        "M=D",],
+        gotoTop: ["@SP", 
+        "A=M-1", ],
+        terminate: ["(END)",
+        "@END",
+        "0;JMP"],
+        compare: (comparison, iterator) => {
+          return [
+            "D=M-D",
+            `@LT_${iterator}`,
+            `D;${comparison}`,
+            `@GT_${iterator}`,
+            "0;JMP",
+            `(LT_${iterator})`,
+            "D=-1",
+            `@STORE_${iterator}`,
+            "0;JMP",
+            `(GT_${iterator})`,
+            "D=0",
+            `(STORE_${iterator})`,
+          ]
+        }
+      }
   }
